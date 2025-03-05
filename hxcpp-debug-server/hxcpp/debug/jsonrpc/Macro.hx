62e08b7bd442c5455d10de62ddf60c667629ded7
Jarrio <Jarrio@users.noreply.github.com>
2025-03-05 11:37:05 +0000

    Update Macro.hx

--- hxcpp-debug-server/hxcpp/debug/jsonrpc/Macro.hx
+++ hxcpp-debug-server/hxcpp/debug/jsonrpc/Macro.hx
@@ -6,7 +6,9 @@ import haxe.macro.Compiler;
 class Macro {
 	macro public static function injectServer():Void {
 		if (Context.defined("cpp") && Context.defined("debug")) {
-			Context.getType("hxcpp.debug.jsonrpc.Server");
+			Context.onAfterInitMacros(() -> {
+				Context.getType("hxcpp.debug.jsonrpc.Server");
+			});
 			Compiler.define("HXCPP_DEBUGGER");
 		}
 	}
